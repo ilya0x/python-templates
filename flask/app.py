cd2ba111776bd0a42752b616a9f5fc791c8becda
ilya0x <ilya.cb@gmail.com>
2023-09-16 13:24:51 -0700

    worked on Flask template

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
     return render_template('index.html')
 
 
